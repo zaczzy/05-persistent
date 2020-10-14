36efa2882e1072790252a2ecb9a3bc7d284ecb41
Stephanie Weirich <sweirich@cis.upenn.edu>
2020-10-14 10:28:25 -0400

    in-class exercise

---
+++ Queue.hs
@@ -0,0 +1,15 @@
+module Queue where
+
+import Test.QuickCheck
+
+-- replace these definitions with something more appropriate
+data Q = Q
+
+empty :: Q
+enq :: Q
+deq :: Q
+empty = undefined
+
+enq = undefined
+
+deq = undefined
